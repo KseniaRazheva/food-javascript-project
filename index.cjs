64d35ac11bb63c7f2af8b43387e5b16c33ef142c
Ksenia Razheva <pokatskayakseniya@gmail.com>
2024-07-20 19:45:30 +0300

    server

--- index.cjs
+++ index.cjs
@@ -1,15 +1,40 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "src")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  fs.readFile("db.json", (error, data) => {
-    if (error) res.send("error readFile");
-    res.send(data.toString());
-  });
+	res.sendFile(path.join(__dirname, "src", "index.html"));
+});
+
+app.get("/get-data", (req, res) => {
+	fs.readFile("db.json", (error, data) => {
+		if (error) res.send("error readFile");
+		res.send(data.toString());
+	});
+});
+
+app.post("/submit", async (req, res) => {
+	const data = req.body;
+
+	try {
+		const file = await fs.readFile("db.json", "utf8");
+		const db = JSON.parse(file);
+		const id = db[db.length - 1].id + 1;
+		db.push({ id, ...data });
+		console.log(db);
+		const write = await fs.writeFile("db.json", JSON.stringify(db));
+		res.status(200).json(db);
+	} catch (error) {
+		console.log(error);
+	}
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+	console.log(`Example app listening on port ${port}`);
 });
