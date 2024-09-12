4909f7aa302207f7874b819688f187102506d00f
Ksenia Razheva <pokatskayakseniya@gmail.com>
2024-09-12 12:57:00 +0300

    bundle added

--- src/dist/bundle.js
+++ src/dist/bundle.js
@@ -1,9 +1,418 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/modules/cards.cjs":
+/*!**********************************!*\
+  !*** ./src/js/modules/cards.cjs ***!
+  \**********************************/
+/***/ ((module) => {
+
+function cards() {
+	//Используем классы для создания карточек меню
+
+	class MenuCard {
+		constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+			this.src = src;
+			this.alt = alt;
+			this.title = title;
+			this.descr = descr;
+			this.price = price;
+			this.classes = classes;
+			this.parent = document.querySelector(parentSelector);
+			this.transfer = 27;
+			this.changeToUAH();
+		}
+
+		changeToUAH() {
+			this.price = this.price * this.transfer;
+		}
+
+		render() {
+			const element = document.createElement("div");
+
+			if (this.classes.length === 0) {
+				this.classes = "menu__item";
+				element.classList.add(this.classes);
+			} else {
+				this.classes.forEach((className) =>
+					element.classList.add(className)
+				);
+			}
+
+			element.innerHTML = `
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            `;
+			this.parent.append(element);
+		}
+	}
+
+	// getResource("http://localhost:3000/").then((data) => {
+	// 	data.forEach(({ img, altimg, title, descr, price }) => {
+	// 		new MenuCard(
+	// 			img,
+	// 			altimg,
+	// 			title,
+	// 			descr,
+	// 			price,
+	// 			".menu .container"
+	// 		).render();
+	// 	});
+	// });
+
+	async function getResource(url) {
+		let res = await fetch(url);
+
+		if (!res.ok) {
+			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+		}
+
+		return await res.json();
+	}
+
+	getResource("http://localhost:3000/get-data").then((data) =>
+		createCard(data)
+	);
+
+	function createCard(data) {
+		data.menu.forEach(({ img, altimg, title, descr, price }) => {
+			const element = document.createElement("div");
+
+			element.classList.add("menu__item");
+
+			element.innerHTML = `
+	            <img src=${img} alt=${altimg}>
+	            <h3 class="menu__item-subtitle">${title}</h3>
+	            <div class="menu__item-descr">${descr}</div>
+	            <div class="menu__item-divider"></div>
+	            <div class="menu__item-price">
+	                <div class="menu__item-cost">Цена:</div>
+	                <div class="menu__item-total"><span>${price}</span> грн/день</div>
+	            </div>
+	        `;
+			document.querySelector(".menu .container").append(element);
+		});
+	}
+}
+
+module.exports = cards;
+
+
+/***/ }),
+
+/***/ "./src/js/modules/forms.cjs":
+/*!**********************************!*\
+  !*** ./src/js/modules/forms.cjs ***!
+  \**********************************/
+/***/ ((module) => {
+
+function forms() {
+	// Forms
+
+	const forms = document.querySelectorAll("form");
+	const message = {
+		loading: "img/form/spinner.svg",
+		success: "Спасибо! Скоро мы с вами свяжемся",
+		failure: "Что-то пошло не так...",
+	};
+
+	forms.forEach((item) => {
+		postData(item);
+	});
+
+	function postData(form) {
+		form.addEventListener("submit", (e) => {
+			e.preventDefault();
+
+			let statusMessage = document.createElement("img");
+			statusMessage.src = message.loading;
+			statusMessage.style.cssText = `
+				display: block;
+				margin: 0 auto;
+			`;
+			form.insertAdjacentElement("afterend", statusMessage);
+
+			const formData = new FormData(form);
+
+			const object = {};
+			formData.forEach(function (value, key) {
+				object[key] = value;
+			});
+
+			fetch("server.php", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(object),
+			})
+				.then((data) => {
+					console.log(data);
+					showThanksModal(message.success);
+					statusMessage.remove();
+				})
+				.catch(() => {
+					showThanksModal(message.failure);
+				})
+				.finally(() => {
+					form.reset();
+				});
+		});
+	}
+
+	function showThanksModal(message) {
+		const prevModalDialog = document.querySelector(".modal__dialog");
+
+		prevModalDialog.classList.add("hide");
+		openModal();
+
+		const thanksModal = document.createElement("div");
+		thanksModal.classList.add("modal__dialog");
+		thanksModal.innerHTML = `
+			<div class="modal__content">
+				<div class="modal__close" data-close>x</div>
+				<div class="modal__title">${message}</div>
+			</div>
+		`;
+		document.querySelector(".modal").append(thanksModal);
+		setTimeout(() => {
+			thanksModal.remove();
+			prevModalDialog.classList.add("show");
+			prevModalDialog.classList.remove("hide");
+			closeModal();
+		}, 4000);
+	}
+}
+
+module.exports = forms;
+
+
+/***/ }),
+
+/***/ "./src/js/modules/modal.cjs":
+/*!**********************************!*\
+  !*** ./src/js/modules/modal.cjs ***!
+  \**********************************/
+/***/ ((module) => {
+
+function modal() {
+	// Modal
+
+	const modalTrigger = document.querySelectorAll("[data-modal]");
+	const modal = document.querySelector(".modal");
+
+	modalTrigger.forEach((btn) => {
+		btn.addEventListener("click", openModal);
+	});
+
+	function closeModal() {
+		modal.classList.add("hide");
+		modal.classList.remove("show");
+		document.body.style.overflow = "";
+	}
+
+	function openModal() {
+		modal.classList.add("show");
+		modal.classList.remove("hide");
+		document.body.style.overflow = "hidden";
+		clearInterval(modalTimerId);
+	}
+
+	modal.addEventListener("click", (e) => {
+		if (e.target === modal || e.target.getAttribute("data-close") == "") {
+			closeModal();
+		}
+	});
+
+	document.addEventListener("keydown", (e) => {
+		if (e.code === "Escape" && modal.classList.contains("show")) {
+			closeModal();
+		}
+	});
+
+	const modalTimerId = setTimeout(openModal, 300000);
+
+	function showModalByScroll() {
+		if (
+			window.scrollY + document.documentElement.clientHeight >=
+			document.documentElement.scrollHeight
+		) {
+			openModal();
+			window.removeEventListener("scroll", showModalByScroll);
+		}
+	}
+	window.addEventListener("scroll", showModalByScroll);
+}
+
+module.exports = modal;
+
+
+/***/ }),
+
+/***/ "./src/js/modules/tabs.cjs":
+/*!*********************************!*\
+  !*** ./src/js/modules/tabs.cjs ***!
+  \*********************************/
+/***/ ((module) => {
+
+function tabs() {
+	// Tabs
+
+	let tabs = document.querySelectorAll(".tabheader__item"),
+		tabsContent = document.querySelectorAll(".tabcontent"),
+		tabsParent = document.querySelector(".tabheader__items");
+
+	function hideTabContent() {
+		tabsContent.forEach((item) => {
+			item.classList.add("hide");
+			item.classList.remove("show", "fade");
+		});
+
+		tabs.forEach((item) => {
+			item.classList.remove("tabheader__item_active");
+		});
+	}
+
+	function showTabContent(i = 0) {
+		tabsContent[i].classList.add("show", "fade");
+		tabsContent[i].classList.remove("hide");
+		tabs[i].classList.add("tabheader__item_active");
+	}
+
+	hideTabContent();
+	showTabContent();
+
+	tabsParent.addEventListener("click", function (event) {
+		const target = event.target;
+		if (target && target.classList.contains("tabheader__item")) {
+			tabs.forEach((item, i) => {
+				if (target == item) {
+					hideTabContent();
+					showTabContent(i);
+				}
+			});
+		}
+	});
+}
+
+module.exports = tabs;
+
+
+/***/ }),
+
+/***/ "./src/js/modules/timer.cjs":
+/*!**********************************!*\
+  !*** ./src/js/modules/timer.cjs ***!
+  \**********************************/
+/***/ ((module) => {
+
+function timer() {
+	// Timer
+
+	const deadline = "2025-05-02"; //ТУТ СТАВЛЮ ДАТУ МЕРОПРИЯТИЯ
+
+	function getTimeRemaining(endtime) {
+		const t = Date.parse(endtime) - Date.parse(new Date()),
+			days = Math.floor(t / (1000 * 60 * 60 * 24)),
+			seconds = Math.floor((t / 1000) % 60),
+			minutes = Math.floor((t / 1000 / 60) % 60),
+			hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+
+		return {
+			total: t,
+			days: days,
+			hours: hours,
+			minutes: minutes,
+			seconds: seconds,
+		};
+	}
+
+	function getZero(num) {
+		if (num >= 0 && num < 10) {
+			return "0" + num;
+		} else {
+			return num;
+		}
+	} //чтобы сделать нолик перед числами 0-9
+
+	function setClock(selector, endtime) {
+		const timer = document.querySelector(selector),
+			days = timer.querySelector("#days"),
+			hours = timer.querySelector("#hours"),
+			minutes = timer.querySelector("#minutes"),
+			seconds = timer.querySelector("#seconds"),
+			timeInterval = setInterval(updateClock, 1000);
+
+		updateClock(); //чтобы запустить - и не было моргания верскти
+		//(вначале браузер показывает то что в хтмл, потом то что в js)
+
+		function updateClock() {
+			const t = getTimeRemaining(endtime);
+
+			days.innerHTML = getZero(t.days);
+			hours.innerHTML = getZero(t.hours);
+			minutes.innerHTML = getZero(t.minutes);
+			seconds.innerHTML = getZero(t.seconds);
+
+			if (t.total <= 0) {
+				clearInterval(timeInterval);
+			}
+		}
+	}
+
+	setClock(".timer", deadline);
+}
+
+module.exports = timer;
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+(() => {
 "use strict";
-const modal = require("./modules/modal.cjs");
-const tabs = require("./modules/tabs.cjs");
-const timer = require("./modules/timer.cjs");
-const cards = require("./modules/cards.cjs");
-const forms = require("./modules/forms.cjs");
+/*!**************************!*\
+  !*** ./src/js/script.js ***!
+  \**************************/
+
+const modal = __webpack_require__(/*! ./modules/modal.cjs */ "./src/js/modules/modal.cjs");
+const tabs = __webpack_require__(/*! ./modules/tabs.cjs */ "./src/js/modules/tabs.cjs");
+const timer = __webpack_require__(/*! ./modules/timer.cjs */ "./src/js/modules/timer.cjs");
+const cards = __webpack_require__(/*! ./modules/cards.cjs */ "./src/js/modules/cards.cjs");
+const forms = __webpack_require__(/*! ./modules/forms.cjs */ "./src/js/modules/forms.cjs");
 
 window.addEventListener("DOMContentLoaded", function () {
 	modal();
@@ -375,3 +784,9 @@ window.addEventListener("DOMContentLoaded", function () {
 	getDynamicInformation("#weight");
 	getDynamicInformation("#age");
 });
+
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
